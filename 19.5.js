d6333dd637e4ebbf915188fd941cbcdf35a7c210
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-07-29 11:29:13 +0600

    String slice join concat & includes

---
+++ 19.5.js
@@ -0,0 +1,13 @@
+// Looping Technique
+
+// 1. for Loop
+// 2. while Loop
+// 3. do while loop -
+// 4. for of --> array loop korar jonno 
+// 5. for in --> object loop korar jonno
+
+const friends = ['']
+
+
+
+// have to do
--- 19.js
+++ 19.js
@@ -2,3 +2,50 @@
 
 //String is immutable - not changeable
 //Array is mutable - can change Array elements
+
+// 19-2
+// all lower or upper case
+const school = 'RAJ UK Uttara Model School';
+
+console.log(school);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+
+
+// manage white space
+const drink =' water';
+const liquid = 'water';
+
+if(drink.trim() === liquid.trim()){
+    console.log('pani khai')
+}
+else{
+    console.log('pari na')
+}
+
+// 19-3
+
+const address = 'andorkilla';
+const part = address.slice(2, 5);
+console.log(part);
+
+const sentence = 'i am good and hard working';
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+
+const sentenceJoin = ['am', 'good', 'and', 'hard', 'working'];
+console.log(sentenceJoin.join());
+console.log(sentenceJoin.join('-'));
+
+
+const first = 'Abid';
+const last = 'Navid';
+// const fullName = first + last;
+const fullName = first + ' ' + last;
+console.log(fullName);
+
+const fullName2 = first.concat(' ').concat(last);
+console.log(fullName2);
+
+//includes
+console.log(last.includes('x'))
