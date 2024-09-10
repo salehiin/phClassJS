ced2b83cf3ce80420732b62f4f1fd3917d61bbbc
Shoeb Salehin Khan <salehin.ay@gmail.com>
2024-09-10 23:42:28 +0600

    matched, includes, toLowerCase

--- 23_5.js
+++ 23_5.js
@@ -7,3 +7,17 @@ function add(num1, num2){
     console.log(arguments[4])
 }
 add(12, 13, 45, 89, 78)
+
+// 23_5-2 -- have to do 
+
+// matched, includes, toLowerCase
+// how to search any word
+
+const numbers = [45, 65, 23, 98, 19];
+
+for(const number of numbers){
+    console.log(number)
+}
+
+// 23_5-3
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
